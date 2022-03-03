5b357d97c5b52847bfd9424323a8bc85d34b8968
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-03 11:31:09 +0300

    add script.js file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var add = function(number1,number2){return number1 + number2};
+var number1 = prompt('enter number here');
+number1;
+'64'
+var number2 = prompt('enter other number here');
+alert(add(number1,number2));
+var number2 = parseInt(prompt('enter other number here'));
+var number1 = parseInt(prompt('enter other number here'));
+alert(add(number1,number2));
+alert;
+var num3 =parseInt(prompt('enter number')):
+var num3 =parseInt(prompt('enter number'));
+var num4 = parseFloat(prompt('enternumber');
+var num4 = (parseFloat(prompt('enternumber'));
+var num4 = parseFloat(prompt('enternumber'));
+alert(multiply(num3,num4));
