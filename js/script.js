0cda8cc662096b49078d63f1d56de33a3fcddb1d
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-03 12:10:26 +0300

    add script.js changes

--- js/script.js
+++ js/script.js
@@ -7,10 +7,14 @@ alert(add(number1,number2));
 var number2 = parseInt(prompt('enter other number here'));
 var number1 = parseInt(prompt('enter other number here'));
 alert(add(number1,number2));
-alert;
-var num3 =parseInt(prompt('enter number')):
+var multiply = function(num3,num4){
+    return(num3 * num4);
+}
 var num3 =parseInt(prompt('enter number'));
-var num4 = parseFloat(prompt('enternumber');
-var num4 = (parseFloat(prompt('enternumber'));
 var num4 = parseFloat(prompt('enternumber'));
 alert(multiply(num3,num4));
+//subtraction
+var subtract = function(number2,number1){return number2-number1;}
+var number2 = parseInt(prompt('enter number'));
+var number1 = parseInt(prompt('enter number'));
+alert(subtract(number2,number1));
